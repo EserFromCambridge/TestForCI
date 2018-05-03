50f698cefcdbbc9843a87f2693d826105ef4452b
Eser Takmaz <etakmaz@asymptote.co.uk>
2018-05-03 14:36:17 +0100

    testing CI in Travis

---
+++ test_CI.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestCI(unittest.TestCase):
+	
+	def setUp(self):
+		print("some text")
+
+	def test_search_in_python_org(self):
+		print("some more text")
+
+	def tearDown(self):
+		print("some text")
+
+
+if __name__ == "__main__":
+	unittest.main()
+
